7caaac15f45098465c860046a987dfcfc37a0e86
SkupovArt <skripachmaster39@gmail.com>
2022-07-09 15:47:22 +0400

    Нифига не работает, ошибок нет

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "артём");
+{
+    Console.WriteLine("Приветствую, хозяин");
+} 
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

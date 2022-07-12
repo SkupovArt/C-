4b2489e33bb6521c78008426ee0c5976bcf9ddf5
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 19:09:35 +0400

    Еще более непонятная херня

--- Example011_array/Program.cs
+++ Example011_array/Program.cs
@@ -19,7 +19,26 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(ind[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 int[] array = new int[10]; // создай новый массив в котором будет 10 элементов
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
 
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);

a3d3f18334b43e92164cc1e2c025c67c4d38fce4
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 18:49:26 +0400

    Зависает терминал с этой функцией

---
+++ Example010_Meth/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 14, 62, 23, 48, 5234, 46, 3257, 85, 229 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n )
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index++;
+
+}

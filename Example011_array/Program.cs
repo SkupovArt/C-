d15ae05b261efed3e2d458c60261635a54180b4a
SkupovArt <skripachmaster39@gmail.com>
2022-07-12 19:00:49 +0400

    Непонятная херотень, ничего не понятно

---
+++ Example011_array/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+    }
+}
+
+int[] array = new int[10]; // создай новый массив в котором будет 10 элементов
+FillArray(array);
+PrintArray(array);
+
